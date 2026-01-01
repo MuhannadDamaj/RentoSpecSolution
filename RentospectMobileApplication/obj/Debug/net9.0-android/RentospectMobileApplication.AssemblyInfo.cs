311316c31eba4cecea286f15caced2ca67dc6ec9
muhannad <muhannad.damaj@gmail.com>
2026-01-01 18:56:53 +0200

    final push

--- RentospectMobileApplication/obj/Debug/net9.0-android/RentospectMobileApplication.AssemblyInfo.cs
+++ RentospectMobileApplication/obj/Debug/net9.0-android/RentospectMobileApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentospectMobileApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dfc60ebf10376f359010c5e60ff5c726abfd363c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentospectMobileApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentospectMobileApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
